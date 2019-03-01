57a735e1bb50bf705b40039d10183c75f0bf0eda
jmlambert78 <jmlambert78@gmail.com>
2019-03-01 18:41:47 +0100

    adapt to CF & AKS services

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
+// Get CF VCAP credential service values.
+var vcap = JSON.parse(process.env.VCAP_SERVICES);
+// reconstruct the string as issue on the password encoding...
+var mongoDB = 'mongodb://'+vcap["azure-cosmosdb-mongo-account"][0]["credentials"].username+':'+ encodeURIComponent(vcap["azure-cosmosdb-mongo-account"][0]["credentials"].password)+"@"+vcap["azure-cosmosdb-mongo-account"][0]["credentials"].host+':'+vcap["azure-cosmosdb-mongo-account"][0]["credentials"].port+'/?ssl=true&replicaSet=globaldb';
+
+mongoose.connect(mongoDB/*,{useNewUrlParser: true}*/);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -20,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-var port = 1234;
+var port = 8080;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
